a777c2f36435228f532a003f1b907885ec5cb49e
Viet Tran <vtra0041@student.monash.edu>
2025-07-04 23:27:46 +1000

    feat: Created AuthUser class to aabstract away the Firebase User class from the UI

---
+++ lib/services/auth/auth_user.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart' show User;
+import 'package:flutter/foundation.dart';
+
+/// Represents an authenticated user in the application.
+/// This class encapsulates the properties and behaviors of a user as we dont want to expose Firebase User directly to UI components.
+// Immutable notation used to ensure that the properties of AuthUser and its subclasses cannot be changed after initialization.
+@immutable
+class AuthUser {
+  final bool isEmailVerified;
+
+  // Constructor
+  const AuthUser({required this.isEmailVerified});
+
+  // Factory constructor to create an AuthUser from a Firebase User.
+  factory AuthUser.fromFirebase(User user) {
+    return AuthUser(isEmailVerified: user.emailVerified);
+  }
+}
